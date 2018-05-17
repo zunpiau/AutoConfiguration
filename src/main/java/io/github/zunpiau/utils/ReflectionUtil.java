8b6c0441871bcb2aefe4330e751acd5e242bf195
zunpiau <zunpiau@qq.com>
2018-05-17 16:54:00 +0800

    :page_facing_up: Adding license.

--- src/main/java/io/github/zunpiau/utils/ReflectionUtil.java
+++ src/main/java/io/github/zunpiau/utils/ReflectionUtil.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.zunpiau.utils;
 
 import javax.annotation.Nullable;
@@ -22,6 +38,15 @@ public class ReflectionUtil {
         field.set(target, cast(value, valueClass));
     }
 
+    /**
+     * @author Juergen Hoeller
+     * @author Rob Harrop
+     * @author Rod Johnson
+     * @author Costin Leau
+     * @author Sam Brannen
+     * @author Chris Beams
+     * copy from @see <a href="https://docs.spring.io/spring-framework/docs/5.0.5.RELEASE/javadoc-api/org/springframework/util/ReflectionUtils.html#makeAccessible-java.lang.reflect.Field-'>ReflectionUtils.html</a>
+     */
     private static void setAccessible(Field field) {
         if ((!Modifier.isPublic(field.getModifiers()) ||
                 !Modifier.isPublic(field.getDeclaringClass().getModifiers()) ||
